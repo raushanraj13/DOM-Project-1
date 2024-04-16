326b143aae6247a763a9f0945520d78d5aee80c0
Raushan <rraushan723@gmail.com>
2024-04-16 13:51:37 +0530

    created: DOM Project using JS

---
+++ script.js
@@ -0,0 +1,10 @@
+const isEle = document.getElementById("myID");
+
+myID.style.color = "blue";
+myID.style.backgroundColor = "yellow";
+myID.style.margin = "20px";
+myID.style.padding = "10px";
+myID.style.fontSize = "18px";
+myID.style.fontWeight = "bold";
+myID.style.height = "200px";
+myID.style.width = "300px";
